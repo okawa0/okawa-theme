7fb77191b041c2f405bec01d2e89672583665d0a
okawa0 <apple_ui@icloud.com>
2025-08-17 22:22:23 +0900

    2025.8.17 worksセクション追加 大川

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
   <div class="mainvisual">
     <h1 class="mainvisual-title">Portfolio</h1>
   </div>
+  <div class="works-section">
+    <div class="works-contents">
+      <h2 class="works-title">Works</h2>
+    </div>
+  </div>
 </main>
 
 <?php get_footer(); ?>
