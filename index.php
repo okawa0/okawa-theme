abf38cfa528a92ee440969a7ec9630100916a058
okawa0 <apple_ui@icloud.com>
2025-08-28 22:26:21 +0900

    2025.8.28 worksセクションタイトルスタイル調整 大川

--- index.php
+++ index.php
@@ -9,7 +9,10 @@
   </div>
   <div class="works-section fade-zoom">
     <div class="works-contents">
-      <h2 class="works-title">Works</h2>
+      <div class="works-titles">
+        <h2 class="works-title">作品</h2>
+        <h3 class="works-subtitle">Works</h3>
+      </div>
       <div class="work-image">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work-mywork.png" alt="" class="work-image-mywork">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work-mywork-sp.png" alt="" class="work-image-mywork-sp">
