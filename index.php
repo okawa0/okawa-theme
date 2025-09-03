3c2b1d7ee0279aeee39fb899bb93187fb680c894
okawa0 <apple_ui@icloud.com>
2025-09-03 22:38:20 +0900

    2025.9.3 skillsセクション追加 大川

--- index.php
+++ index.php
@@ -19,6 +19,23 @@
       </div>
     </div>
   </div>
+  <div class="skills-section">
+    <div class="skills-contents">
+      <div class="skills-titles">
+        <h2 class="skills-title">技術</h2>
+        <h3 class="skills-subtitle">Skills</h3>
+      </div>
+      <div class="skill-images">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/html-logo.png" alt="htmlのロゴ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/css-logo.png" alt="cssのロゴ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/JavaScript-logo.png" alt="javascriptのロゴ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vscode-logo.png" alt="vscodeのロゴ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/figma-logo.png" alt="figmaのロゴ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/github-logo.png" alt="githubのロゴ">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wordpress-logo.png" alt="wordpressのロゴ">
+          </div>
+      </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
