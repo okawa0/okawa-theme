bad1d6d235fc38748b7f86057d4941139cb260a7
okawa0 <apple_ui@icloud.com>
2025-12-13 00:06:00 +0900

    ヒーローレスポンシブ修正

--- index.php
+++ index.php
@@ -20,18 +20,21 @@
   <section class="hero">
     <div class="hero__inner">
       <!-- タイトル -->
-      <h1 id="hero-title" class="hero__title">
-        <span class="char" style="--char-index: 10">P</span><span class="char" style="--char-index: 11">o</span><span class="char" style="--char-index: 12">r</span><span class="char" style="--char-index: 13">t</span><span class="char" style="--char-index: 14">f</span><span class="char" style="--char-index: 15">o</span><span class="char" style="--char-index: 16">l</span><span class="char" style="--char-index: 17">i</span><span class="char" style="--char-index: 18">o</span>
-</h1>
+      <div class="hero-titles">
+        <h1 id="hero-title" class="hero__title">
+          <span class="char" style="--char-index: 10">P</span><span class="char" style="--char-index: 11">o</span><span class="char" style="--char-index: 12">r</span><span class="char" style="--char-index: 13">t</span><span class="char" style="--char-index: 14">f</span><span class="char" style="--char-index: 15">o</span><span class="char" style="--char-index: 16">l</span><span class="char" style="--char-index: 17">i</span><span class="char" style="--char-index: 18">o</span>
+        </h1>
+        <!-- サブタイトル -->
+        <p id="hero-subtitle" class="hero__subtitle">
+          <span class="char" style="--char-index: 19">b</span><span class="char" style="--char-index: 20">y</span>
+          <span class="char" style="--char-index: 21"> </span><span class="char" style="--char-index: 22">W</span><span class="char" style="--char-index: 23">e</span><span class="char" style="--char-index: 24">b</span><span class="char" style="--char-index: 25"> </span><span class="char" style="--char-index: 26">C</span><span class="char" style="--char-index: 27">o</span><span class="char" style="--char-index: 28">d</span><span class="char" style="--char-index: 29">e</span><span class="char" style="--char-index: 30">r</span>
+          <span class="char" style="--char-index: 31"> </span><span class="char" style="--char-index: 32">O</span><span class="char" style="--char-index:33">k</span><span class="char" style="--char-index: 34">a</span><span class="char" style="--char-index: 35">w</span><span class="char" style="--char-index: 36">a</span>
+        </p>
+      </div>
+
       <!-- 墨跡 -->
       <img src="<?php echo esc_url(get_theme_file_uri('assets/images/brush.webp')); ?>" alt="筆跡" class="hero__brush-image">
       <img src="<?php echo esc_url(get_theme_file_uri('assets/images/brush--sp.webp')); ?>" alt="筆跡" class="hero__brush-image--sp">
-      <!-- サブタイトル -->
-      <p id="hero-subtitle" class="hero__subtitle">
-        <span class="char" style="--char-index: 19">b</span><span class="char" style="--char-index: 20">y</span>
-        <span class="char" style="--char-index: 21"> </span><span class="char" style="--char-index: 22">W</span><span class="char" style="--char-index: 23">e</span><span class="char" style="--char-index: 24">b</span><span class="char" style="--char-index: 25"> </span><span class="char" style="--char-index: 26">C</span><span class="char" style="--char-index: 27">o</span><span class="char" style="--char-index: 28">d</span><span class="char" style="--char-index: 29">e</span><span class="char" style="--char-index: 30">r</span>
-        <span class="char" style="--char-index: 31"> </span><span class="char" style="--char-index: 32">O</span><span class="char" style="--char-index:33">k</span><span class="char" style="--char-index: 34">a</span><span class="char" style="--char-index: 35">w</span><span class="char" style="--char-index: 36">a</span>
-      </p>
     </div>
   </section>
 
@@ -68,7 +71,7 @@
               <p>
             </div>
           </article>
-</li>
+        </li>
         <li class="works__item reveal">
           <article>
             <a href="<?php echo esc_url('https://okawa0.github.io/interior-store/index.html'); ?>" target="_blank" rel="noopener noreferrer">
@@ -86,7 +89,7 @@
               <p>
             </div>
           </article>
-</li>
+        </li>
         <li class="works__item reveal">
           <article>
             <a href="<?php echo esc_url('https://okawa-web.com/'); ?>" target="_blank" rel="noopener noreferrer">
@@ -104,7 +107,7 @@
               <p>
             </div>
           </article>
-</li>
+        </li>
       </ul>
     </div>
   </section>
