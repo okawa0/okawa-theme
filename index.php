86598d97ceef73e1b4f85ee337037a05459f03a9
Makoto Okawa <167591230+okawa0@users.noreply.github.com>
2025-12-22 12:18:19 +0900

    自己紹介文修正

--- index.php
+++ index.php
@@ -193,11 +193,10 @@
           alt="大川誠のプロフィール画像"
           class="about__image reveal">
         <div class="about__info reveal">
-          <p class="about__info__name">大川 誠（Okawa Makoto）|　Webコーダー</p>
-          <p>1992年10月生まれ。愛知県在住。趣味は登山。猫飼い。</p>
+          <p class="about__info__name">おおかわ|　Webコーダー</p>
           <p>現在Web制作の現場でコーディング業務に取り組んでいます。</p>
           <p>コードでデザインを正確に再現し、見る人に心地よさを感じてもらえるサイトづくりを目指しています。</p>
-          <p>日々、より良いコードを書くために学びを重ね、実案件や模写を通して実装力を高めています。</p>
+          <p>日々、より良いコードを書くために学びを重ね、実案件や仮想案件を通して実装力を高めています。</p>
           <div class="social-links">
             <a href="https://github.com/okawa0" target="_blank">
               <img src="<?php echo esc_url(get_theme_file_uri('assets/images/github-logo.png')); ?>" alt="GitHub" class="social-links__icon">
