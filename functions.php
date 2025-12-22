8938a2310cf546ef853f2bb13c27146ef117a4ab
okawa0 <apple_ui@icloud.com>
2025-12-22 22:26:18 +0900

    enqueueの集約

--- functions.php
+++ functions.php
@@ -3,18 +3,6 @@
 // タイトルタグの自動生成
 add_theme_support('title-tag');
 
-// CSSの読み込み
-function my_theme_enqueue_styles()
-{
-  wp_enqueue_style(
-    'main-style',
-    get_template_directory_uri() . '/assets/css/style.css',
-    // キャッシュクリア
-    filemtime(get_template_directory() . '/assets/css/style.css')
-  );
-}
-// フック
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 // サムネイル
 add_theme_support('/assets/images/thumbnail.webp');
 
@@ -29,19 +17,6 @@ function my_theme_enqueue_fonts()
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_fonts');
 
-// JSの読み込み
-function mytheme_enqueue_scripts()
-{
-  wp_enqueue_script(
-    'mytheme-script',
-    get_template_directory_uri() . '/assets/js/script.js',
-    array(),
-    '1.0.0',
-    true
-  );
-}
-add_action('wp_enqueue_scripts', 'mytheme_enqueue_scripts');
-
 // ページ読み込み後スクリプト実行
 function add_defer_attribute($tag, $handle, $src)
 {
@@ -52,10 +27,24 @@ function add_defer_attribute($tag, $handle, $src)
 }
 add_filter('script_loader_tag', 'add_defer_attribute', 10, 3);
 
-// キャッシュの無効化
-wp_enqueue_style(
-  'main-style',
-  get_template_directory_uri() . '/assets/css/style.css',
-  [],
-  filemtime(get_template_directory() . '/assets/css/style.css')
-);
+
+function my_theme_enqueue_assets(){
+
+  // CSS
+  wp_enqueue_style(
+    'main-style',
+    get_template_directory_uri() . '/assets/css/style.css',
+    [],
+    filemtime(get_template_directory() . '/assets/css/style.css')
+  );
+
+  // JS
+  wp_enqueue_script(
+    'mytheme-script',
+    get_template_directory_uri() . '/assets/js/script.js',
+    [],
+    filemtime(get_template_directory() . '/assets/js/script.js'),
+    true
+  );
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_assets');
