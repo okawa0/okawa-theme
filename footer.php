8890c97c808c82623487b0e475232e6ff1e5d124
okawa0 <apple_ui@icloud.com>
2026-02-07 11:41:21 +0900

    Top リンクを # から home_url('/') に変更（ホームへのリンクに統一）

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
   <footer class="site-footer">
     <div class="footer__inner">
       <ul class="footer__nav">
-        <li><a href="#" class="u-underline u-underline--whiteshadow">Top</a></li>
+        <li><a href="<?php echo esc_url(home_url('/')); ?>" class="u-underline u-underline--whiteshadow">Top</a></li>
         <li><a href="#about" class="u-underline u-underline--whiteshadow">About</a></li>
         <li><a href="#skills" class="u-underline u-underline--whiteshadow">Skills</a></li>
         <li><a href="#works" class="u-underline u-underline--whiteshadow">Works</a></li>
@@ -11,7 +11,6 @@
     </div>
   </footer>
 
-  <!-- wp_footer() = プラグインやテーマが必要な連絡事項（スクリプト）を自動で差し込むための場所（プラグインからのスクリプトの受け皿） -->
   <?php wp_footer(); ?>
 
 </body>
